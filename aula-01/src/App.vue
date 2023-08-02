ee1e1b09d1af03ac56db81cf095e460001849cb2
Bruno-Costa-fig <brunocosta.fig@gmail.com>
2023-08-01 22:04:07 -0300

    códgio aula 02

---
+++ aula-01/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- SINGLE FILE COMPONENT -->
+  <div class="container">
+    <h1>Hello, world!</h1>
+    <h2>{{ nome }}</h2>
+    <input type="text" v-model="nome" />
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return { 
+      nome: "Bruno costa",
+      usuario: {
+        idade: 24,
+        email: "email@email.com"
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  width: 100vw;
+  height: 100vh;
+  background-color: rgb(244, 129, 129);
+}
+</style>
