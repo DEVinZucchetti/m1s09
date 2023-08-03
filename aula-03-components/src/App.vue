ac17f7291968d300e83a6c81df9ca388869e5bdc
Bruno-Costa-fig <brunocosta.fig@gmail.com>
2023-08-02 21:58:29 -0300

    codigo aula 03

---
+++ aula-03-components/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- utilizando o componente -->
+  <Card>
+    <template #header>
+      <h1>Carro novo da Ferrari</h1>
+    </template>
+    <template #imagem>
+      <img :width="200" src="https://media.formula1.com/content/dam/fom-website/sutton/2022/Brazil/Friday/1440800084.jpg" alt="">
+        
+    </template>
+  </Card>
+</template>
+
+<script>
+// importando o componente
+import Card from "./components/Card.vue"
+
+export default {
+  // registrando o componente
+  components: {
+    Card
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ aula-03-components/src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <slot name="header">
+      <h1>Carro Ferrari</h1>
+    </slot>
+  </header>
+  <slot name="imagem">
+    <img :width="200" src="https://silodrome.com/wp-content/uploads/2022/05/Nigel-Mansell-Ferrari-640-F1-Car.jpg" alt="carro ferrari">
+  </slot>
+  <slot></slot>
+
+  <footer>
+    <slot name="footer">
+      <h6>Carro F1 640 - 1989</h6>
+    </slot>
+  </footer>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
